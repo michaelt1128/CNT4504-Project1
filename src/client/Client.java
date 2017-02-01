d2404643f91f5a1d80523394e1a8f3d084cb70ab
michaelt1128 <michael.turner1128@gmail.com>
2017-02-01 16:03:00 -0500

    Setting up client/server

--- src/client/Client.java
+++ src/client/Client.java
@@ -9,14 +9,31 @@ public class Client {
 		int portNumber = 5012;
 		try (
 			Socket echoSocket = new Socket(hostName, portNumber);
+			
+			// out : message sent to server
 			PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+			//in : message received from server
 			BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+			//stdIn : user's message typed in console
 			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
 		) {
 			String userInput;
 			while ((userInput = stdIn.readLine()) != null) {
+				
+				// Print user input to socket
 				out.println(userInput);
-				System.out.println("echo: " + in.readLine());
+
+				System.out.println("Server response:");
+				
+				// Reads and prints all lines in server response
+				String serverRes;
+				while ((serverRes = in.readLine()).length() > 0) {
+					if(serverRes.equals("end")) {
+						break;
+					} else {
+						System.out.println(serverRes);
+					}
+				}
 			}
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host " + hostName);
--- src/server/Server.java
+++ src/server/Server.java
@@ -2,19 +2,38 @@ package server;
 
 import java.net.*;
 import java.io.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class Server {
     public static void main(String[] args) throws IOException {	
         int portNumber = 5012;
         try (
+        	// Creates socket
             ServerSocket serverSocket = new ServerSocket(portNumber);
             Socket clientSocket = serverSocket.accept();     
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
+        		
+        	// Prints to socket
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); 
+        	
+        	// Receives from socket
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         ) {
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
-                out.println(inputLine);
+            	String outputStr = "Received string: " + inputLine + "\n";
+                switch (inputLine){
+                    //add api cases here
+                    case "date": 
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                        outputStr = outputStr + dateFormat.format(new Date());
+                        break;
+                    default:
+                        out.println("Error: not a valid input");
+                        break;
+                }
+                //appends 'end' to mark end of transmission
+                out.println(outputStr + "\nend");
             }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
