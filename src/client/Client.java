74cfeaced0e499bd8bb1e66a1ad1afd597c3eaad
michaelt1128 <michael.turner1128@gmail.com>
2017-02-08 18:26:32 -0500

    stuff

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,25 +2,27 @@ package client;
 
 import java.io.*;
 import java.net.*;
+import java.util.Date;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
+		
 		String hostName = "127.0.0.1";
 		int portNumber = 5012;
+		
 		try (
-			Socket echoSocket = new Socket(hostName, portNumber);
-			
+			Socket serverSocket = new Socket(hostName, portNumber);
 			// out : message sent to server
-			PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+			PrintWriter out = new PrintWriter(serverSocket.getOutputStream(), true);
 			//in : message received from server
-			BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
 			//stdIn : user's message typed in console
 			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
 		) {
 			String userInput;
 			while ((userInput = stdIn.readLine()) != null) {
-				
 				// Print user input to socket
+				Date start = new Date();
 				out.println(userInput);
 
 				System.out.println("Server response:");
@@ -34,6 +36,8 @@ public class Client {
 						System.out.println(serverRes);
 					}
 				}
+				Date end = new Date();
+				System.out.println(end.getTime()-start.getTime()+ "ms");
 			}
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host " + hostName);
