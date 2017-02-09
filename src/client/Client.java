8db43ca36310b1506c58c763e191ae251ee5ecfb
Unknown <matwaroff@gmail.com>
2017-02-09 14:46:14 -0500

    client gui

--- src/client/Client.java
+++ src/client/Client.java
@@ -3,48 +3,71 @@ package client;
 import java.io.*;
 import java.net.*;
 import java.util.Date;
+import java.awt.*;
+import javax.swing.*;
+import java.lang.StringBuilder;
 
 public class Client {
-	public static void main(String[] args) throws IOException {
-		
-		String hostName = "127.0.0.1";
-		int portNumber = 5012;
-		
-		try (
-			Socket serverSocket = new Socket(hostName, portNumber);
-			// out : message sent to server
-			PrintWriter out = new PrintWriter(serverSocket.getOutputStream(), true);
-			//in : message received from server
-			BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
-			//stdIn : user's message typed in console
-			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
-		) {
-			String userInput;
-			while ((userInput = stdIn.readLine()) != null) {
-				// Print user input to socket
-				Date start = new Date();
-				out.println(userInput);
 
-				System.out.println("Server response:");
-				
-				// Reads and prints all lines in server response
-				String serverRes;
-				while ((serverRes = in.readLine()).length() > 0) {
-					if(serverRes.equals("end")) {
-						break;
-					} else {
-						System.out.println(serverRes);
-					}
-				}
-				Date end = new Date();
-				System.out.println(end.getTime()-start.getTime()+ "ms");
-			}
-		} catch (UnknownHostException e) {
-			System.err.println("Don't know about host " + hostName);
-			System.exit(1);
-		} catch (IOException e) {
-			System.err.println("Couldn't get I/O for the connection to " + hostName);
-			System.exit(1);
-		}
-	}
+    private Socket serverSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private BufferedReader stdIn;
+    public String serverResponse;
+    public boolean connected;
+    private String hostName;
+    private int portNumber;
+
+    public Client(String hostname, int port) {
+        hostName = hostname;
+        portNumber = port;
+        connect();
+    }
+
+    public String userInput(String userInput) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        while (userInput != null) {
+            // Print user input to socket
+            Date start = new Date();
+            System.out.println("Server response:");
+            out.println(userInput);
+            
+            // Reads and prints all lines in server response
+            String serverRes;
+            while ((serverRes = in.readLine()).length() > 0) {
+                System.out.println(serverRes);
+                if (serverRes.equals("end")){
+                    break;
+                } else {
+                    System.out.println(serverRes);
+                    sb.append(serverRes + "\n");
+                }
+            }
+            Date end = new Date();
+            sb.append((end.getTime() - start.getTime()) + "ms");
+            userInput = null;
+        }
+        return sb.toString();
+    }
+
+    public void connect() {
+        try {
+                serverSocket = new Socket(hostName, portNumber);
+                // out : message sent to server
+                out = new PrintWriter(serverSocket.getOutputStream(), true);
+                //in : message received from server
+                in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+                //stdIn : user's message typed in console
+                stdIn = new BufferedReader(new InputStreamReader(System.in));
+                connected = true;
+        }
+        catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        }
+        catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
+        }
+    }
 }
