783116ff9d74913e02a4e56cea9fc234f5758729
michaelt1128 <michael.turner1128@gmail.com>
2017-02-08 19:55:13 -0500

    Fixed server shutting off

--- src/server/Server.java
+++ src/server/Server.java
@@ -6,54 +6,57 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class Server {
-    public static void main(String[] args) throws IOException {	
-        int portNumber = 5012;
-        try (
-        	// Creates socket
-            ServerSocket serverSocket = new ServerSocket(portNumber);
-        	
-            Socket clientSocket = serverSocket.accept();     
-        		
-        	// Prints to socket
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); 
-        	
-        	// Receives from socket
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-            	String outputStr = "Received string: " + inputLine + "\n";
-                switch (inputLine){
-                    //add api cases here
-                    case "date": 
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-                        outputStr += dateFormat.format(new Date());
-                        break;
-                    case "uptime":
-                    	break;
-                    case "memory":
-                    	break;
-                    case "netstat":
-                    	break;
-                    case "users":
-                    	break;
-                    case "processes":
-                    	break;
-                    case "close":
-                    	clientSocket.close();
-                    	outputStr += "Closed socket";
-                    	break;
-                    default:
-                        outputStr += "Error: not a valid input";
-                        break;
-                }
-                //appends 'end' to mark end of transmission
-                out.println(outputStr + "\nend");
-            }
-        } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                + portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("Connecting to server");
+		
+		boolean connect = true;
+		while (connect) {
+			int portNumber = 5012;
+			try (
+					// Creates socket
+					ServerSocket serverSocket = new ServerSocket(portNumber);
+					Socket clientSocket = serverSocket.accept();
+					// Prints to socket
+					PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+					// Receives from socket
+					BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			) {
+				String inputLine;
+				while ((inputLine = in.readLine()) != null) {
+					String outputStr = "Received string: " + inputLine + "\n";
+					switch (inputLine) {
+						// add api cases here
+						case "date":
+							SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+							outputStr += dateFormat.format(new Date());
+							break;
+						case "uptime":
+							break;
+						case "memory":
+							break;
+						case "netstat":
+							break;
+						case "users":
+							break;
+						case "processes":
+							break;
+						case "close":
+							clientSocket.close();
+							outputStr += "Closed socket";
+							break;
+						default:
+							outputStr += "Error: not a valid input";
+							break;
+					}
+					// appends 'end' to mark end of transmission
+					out.println(outputStr + "\nend");
+				}
+			} catch (IOException e) {
+				System.out.println("Exception caught when trying to listen on port " + portNumber
+						+ " or listening for a connection");
+				System.out.println(e.getMessage());
+			}
+		}
+	}
 }
